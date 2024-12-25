ab7c0c811d2fd4a7f7332c61bcc62c71b529f132
dddomain <xaxa.reira2306@icloud.com>
2024-12-25 14:29:54 +0900

    Initial Commit

---
+++ SharedShoppingList/SharedShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedShoppingListApp.swift
+//  SharedShoppingList
+//
+//  Created by 小原大輔 on 2024/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SharedShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

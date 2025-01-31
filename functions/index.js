39c9f8e8b429a0ed73ac2e2c67b74cb77c639e67
dddomain <xaxa.reira2306@icloud.com>
2025-01-31 21:37:46 +0900

    HTTPv1による認証を実装中。

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,8 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const { google } = require("googleapis");
 
-admin.initializeApp();
+admin.initializeApp(); // service-account.json を不要にした
 
-// Firebase のプロジェクトID
-const PROJECT_ID = "sharedshoppinglist-feecd";
-const MESSAGING_SCOPE = "https://www.googleapis.com/auth/firebase.messaging";
-const SCOPES = [MESSAGING_SCOPE];
-
-// Google 認証用のJWTを取得
-async function getAccessToken() {
-    const key = require("./service-account.json");  // ここでダウンロードした JSON を参照
-    const jwtClient = new google.auth.JWT(
-        key.client_email,
-        null,
-        key.private_key,
-        SCOPES,
-        null
-    );
-    const tokens = await jwtClient.authorize();
-    return tokens.access_token;
-}
-
-// プッシュ通知を送信するHTTP関数
 exports.sendPushNotification = functions.https.onRequest(async (req, res) => {
     const { token, title, body } = req.body;
 
@@ -32,29 +11,17 @@ exports.sendPushNotification = functions.https.onRequest(async (req, res) => {
     }
 
     try {
-        const accessToken = await getAccessToken();
         const message = {
-            message: {
-                token: token,
-                notification: {
-                    title: title,
-                    body: body
-                }
-            }
+            token: token,
+            notification: {
+                title: title,
+                body: body,
+            },
         };
 
-        const response = await fetch(`https://fcm.googleapis.com/v1/projects/sharedshoppinglist-feecd/messages:send`, {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${accessToken}`,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(message)
-        });
-
-        const data = await response.json();
-        console.log("Push notification sent:", data);
-        res.status(200).send(data);
+        const response = await admin.messaging().send(message);
+        console.log("Push notification sent:", response);
+        res.status(200).send(response);
     } catch (error) {
         console.error("Error sending push notification:", error);
         res.status(500).send("Error sending notification");

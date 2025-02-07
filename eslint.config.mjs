21a7e6be5de36853c58951c822250173583ea65a
dddomain <xaxa.reira2306@icloud.com>
2025-02-07 18:29:09 +0900

    キャッシュ消去

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import globals from "globals/index.js";
-import pluginJs from "@eslint/js";
-
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
